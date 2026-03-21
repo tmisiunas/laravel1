a71ad8ab50f0ecc5b30330872b7573c55f360357
Misiūnas <tmisiunas@gmail.com>
2026-03-21 03:29:08 +0200

    Update header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="flex items-center justify-start gap-4">
     <a
         href="{{ route('hello') }}"
-        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal {{ request()->routeIs('hello') ? 'font-bold underline' : '' }}"
     >
         Hello World
     </a>
