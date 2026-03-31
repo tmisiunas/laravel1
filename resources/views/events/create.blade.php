ba3a7f3820781cdbcc9d19d7aca388f67cb41fb3
Misiūnas <tmisiunas@gmail.com>
2026-03-31 04:26:28 +0300

    Event create form

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,50 +1,71 @@
-<h2>Add Event</h2>
+    <h2>Create Event</h2>
 
-<form method="POST" action="{{ route('events.store') }}">
-    @csrf
+    @if(session('success'))
+        <p style="color: green">{{ session('success') }}</p>
+    @endif
 
-    <!-- Name -->
-    <div>
-        <label>Name:</label><br>
-        <input type="text" name="participant">
-    </div>
+    <form method="POST" action="{{ route('events.store') }}">
+        @csrf
 
-    <!-- Country -->
-    <div>
-        <label>Country:</label><br>
-        <select name="country_id">
-            @foreach($countries as $country)
-                <option value="{{ $country->id }}">
-                    {{ $country->name }}
-                </option>
-            @endforeach
-        </select>
-    </div>
-
-    <!-- Sport -->
-    <div>
-        <label>Sport:</label><br>
+        <!-- Sport -->
         <select name="sport_id">
+            <option value="">Select Sport</option>
             @foreach($sports as $sport)
-                <option value="{{ $sport->id }}">
-                    {{ $sport->sport }}
-                </option>
+                <option value="{{ $sport->id }}">{{ $sport->sport }}</option>
             @endforeach
         </select>
-    </div>
 
-    <!-- Participant Type -->
-    <div>
-        <label>Type:</label><br>
-        <select name="participant_type_id">
-            @foreach($types as $type)
-                <option value="{{ $type->id }}">
-                    {{ $type->type }}
-                </option>
+        <br><br>
+
+        <!-- Contest -->
+        <select name="contest_id">
+            <option value="">Select Contest</option>
+            @foreach($contests as $contest)
+                <option value="{{ $contest->id }}">{{ $contest->name }}</option>
             @endforeach
         </select>
-    </div>
 
-    <br>
-    <button type="submit">Save</button>
-</form>
+        <br><br>
+
+        <!-- Participant 1 -->
+        <select name="participant1_id">
+            <option value="">Participant 1</option>
+            @foreach($participants as $p)
+                <option value="{{ $p->id }}">{{ $p->participant }}</option>
+            @endforeach
+        </select>
+
+        <br><br>
+
+        <!-- Participant 2 -->
+        <select name="participant2_id">
+            <option value="">Participant 2</option>
+            @foreach($participants as $p)
+                <option value="{{ $p->id }}">{{ $p->participant }}</option>
+            @endforeach
+        </select>
+
+        <br><br>
+
+        <!-- Time -->
+        <input type="datetime-local" name="event_start_time">
+
+        <br><br>
+
+        <!-- Value -->
+        <input type="number" name="value" placeholder="Value">
+
+        <br><br>
+
+        <!-- Result -->
+        <input type="number" name="result" placeholder="Result">
+
+        <br><br>
+
+        <!-- Result text -->
+        <input type="text" name="result_text" placeholder="Result Text">
+
+        <br><br>
+
+        <button type="submit">Save</button>
+    </form>
