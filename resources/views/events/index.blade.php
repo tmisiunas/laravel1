4173f97773c990e5aeaf39fd7cd11533dab454e1
Misiūnas <tmisiunas@gmail.com>
2026-04-20 04:06:47 +0300

    Update index.blade.php

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -9,8 +9,11 @@
     <table border="1" cellpadding="5">
         <tr>
             <th>Id</th>
-            <th>Sports Id</th>
-            <th>Contest Id</th>
+            <th>Sport</th>
+            <th>Contest</th>
+            <th>Start time</th>
+            <th>Participant 1</th>
+            <th>Participant 2</th>
         </tr>
 
         @foreach($events as $e)
@@ -18,6 +21,9 @@
                 <td>{{ $e->id }}</td>
                 <td>{{ $e->sport->sport }}</td>
                 <td>{{ $e->contest->name }}</td>
+                <td>{{ $e->event_start_time }}</td>
+                <td>{{ $e->participant1->participant }}</td>
+                <td>{{ $e->participant2->participant }}</td>
             </tr>
         @endforeach
     </table>
