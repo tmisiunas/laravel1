ec6b0e64e43ee4a1241b18e512e81ac0be299a32
Misiūnas <tmisiunas@gmail.com>
2026-04-20 06:30:24 +0300

    Update index.blade.php

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -25,7 +25,7 @@
 
             <tbody>
             @foreach($events as $e)
-                <tr>
+                <tr class="odd:bg-neutral-primary even:bg-red border-b border-default">
                     <td class="px-10 py-2">{{ $e->id }}</td>
                     <td class="px-10 py-2">{{ $e->sport->sport }}</td>
                     <td class="px-10 py-2">{{ $e->contest->name }}</td>
