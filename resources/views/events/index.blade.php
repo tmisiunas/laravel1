e52e39ed4f12cf97ffebb521d44493797c9fa610
Misiūnas <tmisiunas@gmail.com>
2026-04-21 04:25:59 +0300

    Tailwind and Vite is added

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -12,20 +12,21 @@
 
     <div class="max-w-5xl mx-auto mt-6">
         <table class="min-w-full rounded-lg overflow-hidden">
-            <thead class="bg-neutral-100">
-            <tr class="border-t odd:bg-white even:bg-gray-50 hover:bg-gray-100">
-                <th class="px-10 py-2 text-left text-sm font-medium">Id</th>
-                <th class="px-10 py-2 text-left text-sm font-medium">Sport</th>
-                <th class="px-10 py-2 text-left text-sm font-medium">Contest</th>
-                <th class="px-10 py-2 text-left text-sm font-medium">Start time</th>
-                <th class="px-10 py-2 text-left text-sm font-medium">Participant 1</th>
-                <th class="px-10 py-2 text-left text-sm font-medium">Participant 2</th>
+
+            <thead class="bg-gray-100">
+            <tr>
+                <th class="px-10 py-2 text-left text-sm font-bold text-gray-700">Id</th>
+                <th class="px-10 py-2 text-left text-sm font-bold text-gray-700">Sport</th>
+                <th class="px-10 py-2 text-left text-sm font-bold text-gray-700">Contest</th>
+                <th class="px-10 py-2 text-left text-sm font-bold text-gray-700">Start time</th>
+                <th class="px-10 py-2 text-left text-sm font-bold text-gray-700">Participant 1</th>
+                <th class="px-10 py-2 text-left text-sm font-bold text-gray-700">Participant 2</th>
             </tr>
             </thead>
-
+            
             <tbody>
             @foreach($events as $e)
-                <tr class="odd:bg-gray-50 even:bg-white hover:bg-indigo-50 transition">
+                <tr class="odd:bg-gray-200 even:bg-white hover:bg-indigo-50">
                     <td class="px-10 py-2">{{ $e->id }}</td>
                     <td class="px-10 py-2">{{ $e->sport->sport }}</td>
                     <td class="px-10 py-2">{{ $e->contest->name }}</td>
